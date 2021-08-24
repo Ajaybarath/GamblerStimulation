d28aa5d9c55bcf43070847a08d619a9383b95e3f
ajaybarath <ajaybarath3@gmail.com>
2021-08-24 09:12:07 +0530

    ADDED: exit if won or lost 50% condition added

--- src/com/bridgeLabz/GamblersGame/GamblingGame.java
+++ src/com/bridgeLabz/GamblersGame/GamblingGame.java
@@ -5,21 +5,48 @@ public class GamblingGame {
 	static final int INITIAL_STAKE = 100;
 	static final int EVERY_GAME_STAKE = 1;
 	
+	int gamesWon = 0;
+	int gamesLost = 0;
+	
 	public static void main(String args[]) {
 		
 
 		System.out.println("Money at stake for every game : " + EVERY_GAME_STAKE);
 		System.out.println("Money at stake for every day : " + INITIAL_STAKE);
 		
+		GamblingGame player = new GamblingGame();
+		player.exitLogic();
+ 		
+	}
+	
+	public void gamePlay() {
+		
 		int gameStatus = (int) (Math.random()*10) % 2;
 		
 		if (gameStatus == 1) {
+			gamesWon ++;
 			System.out.println("Gambler won 1 $");
 		}
 		else {
+			gamesLost ++;
 			System.out.println("Gambler lost 1 $");
 		}
- 		
+		
 	}
-
+	
+	public void exitLogic() {
+		
+		while(gamesWon < 50 && gamesLost < 50) {
+			gamePlay();
+		}
+		
+		if (gamesWon == 50) {
+			System.out.println("Player resigned by winning 50$");
+		}
+		else {
+			System.out.println("Player resigned by loosing 50$");
+		}
+	}
+ 
+	
 }
