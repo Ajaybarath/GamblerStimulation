8297320f724651c6d1682901b3b03df054a95b7f
ajaybarath <ajaybarath3@gmail.com>
2021-08-24 08:41:36 +0530

    ADDED: converted local varible to global constants

--- src/com/bridgeLabz/GamblersGame/GamblingGame.java
+++ src/com/bridgeLabz/GamblersGame/GamblingGame.java
@@ -1,5 +1,14 @@
 package com.bridgeLabz.GamblersGame;
 
 public class GamblingGame {
+	
+	static final int INITIAL_STAKE = 100;
+	static final int EVERY_GAME_STAKE = 1;
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Money at stake for every game : " + EVERY_GAME_STAKE);
+		System.out.println("Money at stake for every day : " + INITIAL_STAKE);
+	}
 
 }
